81ad4427db5a496c98fb7b3856d71e4991a77f70
MeftunH <meftun.hesimli@epigra.com>
2022-02-06 22:56:40 +0300

    lombok implementation

--- src/main/java/codesIO/northwind/entities/concretes/Product.java
+++ src/main/java/codesIO/northwind/entities/concretes/Product.java
@@ -1,14 +1,19 @@
 package codesIO.northwind.entities.concretes;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Entity // Annotation İşlemi
-@Table(name = "products") // Veritabanı İşlemi
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
 public class Product {
-    @Id //İşlemeri ID'ye göre yapak
-    @GeneratedValue // Bir bir arttırılacak
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     @Column(name = "product_id")
     private int id;
@@ -26,65 +31,5 @@ public class Product {
     private short unitsInStock;
 
     @Column(name = "quantity_per_unit")
-    private String quantityPerUnit; // Bir ünitede kaç tane var.
-
-
-    public Product() {}
-
-    public Product(int id, int categoryId, String productName, double unitPrice, short unitsInStock, String quantityPerUnit) {
-        this.id = id;
-        this.categoryId = categoryId;
-        this.productName = productName;
-        this.unitPrice = unitPrice;
-        this.unitsInStock = unitsInStock;
-        this.quantityPerUnit = quantityPerUnit;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public double getUnitPrice() {
-        return unitPrice;
-    }
-
-    public void setUnitPrice(double unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
-    public short getUnitsInStock() {
-        return unitsInStock;
-    }
-
-    public void setUnitsInStock(short unitsInStock) {
-        this.unitsInStock = unitsInStock;
-    }
-
-    public String getQuantityPerUnit() {
-        return quantityPerUnit;
-    }
-
-    public void setQuantityPerUnit(String quantityPerUnit) {
-        this.quantityPerUnit = quantityPerUnit;
-    }
+    private String quantityPerUnit;
 }
